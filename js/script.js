7795f7c6d5ece89be82d34480a9ef7aa65ee6092
Kassio Kennedy <154475006+Kassio07@users.noreply.github.com>
2024-08-03 22:28:10 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -3,15 +3,14 @@
 let c = (a)=>document.querySelector(a);
 let cs = (a)=>document.querySelectorAll(a);
 
-// Controls
 let hamburguer = c('.btnHamburguer');
 let menu = c('.menuNav');
 
-// fazer o menu open close
 
 hamburguer.addEventListener('click', ()=>{
-
-    menu.classList.toggle(openModal)
+    menu.classList.toggle('openModal')
     
 });
+
+
 
