6e68eb2e4790067d5c1ee161cb027b4f81933441
CodeKaess <154475006+Kassio07@users.noreply.github.com>
2024-08-05 15:42:01 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -8,6 +8,18 @@ let hamburguer = c(".btnHamburguer");
 let menu = c(".menuNav");
 
 hamburguer.addEventListener("click", () => {
-  logo.classList.toggle("noneImg");
-  menu.classList.toggle("openModal");
+    menu.classList.toggle("openModal");
+    logo.classList.toggle("noneImg");
+
+    menu.style.opacity = 0;
+    setTimeout(()=>{
+        if(menu.classList.contains('openModal')){
+            c('.fa-bars').style.display = 'none'
+            c('.fa-circle-xmark').style.display = 'flex'
+             menu.style.opacity = 1;
+        }else{
+            c('.fa-bars').style.display = 'flex'
+            c('.fa-circle-xmark').style.display = 'none'
+        }
+    },200)
 });
