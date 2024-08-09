dbe71c293c853b82a22ab13243dc633f77ea87e8
CodeKaess <154475006+Kassio07@users.noreply.github.com>
2024-08-09 15:29:01 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -8,40 +8,54 @@ let hamburguer = c(".btnHamburguer");
 let menu = c(".menuNav");
 
 hamburguer.addEventListener("click", () => {
-    menu.classList.toggle("openModal");
-    logo.classList.toggle("noneImg");
+  menu.classList.toggle("openModal");
+  logo.classList.toggle("noneImg");
 
-    menu.style.opacity = 0;
-    setTimeout(()=>{
-        if(menu.classList.contains('openModal')){
-            c('.fa-bars').style.display = 'none'
-            c('.fa-circle-xmark').style.display = 'flex'
-             menu.style.opacity = 1;
-        }else{
-            c('.fa-bars').style.display = 'flex'
-            c('.fa-circle-xmark').style.display = 'none'
-        }
-    },200)
+  menu.style.opacity = 0;
+  setTimeout(() => {
+    if (menu.classList.contains("openModal")) {
+      c(".fa-bars").style.display = "none";
+      c(".fa-circle-xmark").style.display = "flex";
+      menu.style.opacity = 1;
+    } else {
+      c(".fa-bars").style.display = "flex";
+      c(".fa-circle-xmark").style.display = "none";
+    }
+  }, 200);
 });
 
-const frame = c('.video video');
-function openClose(){
-    const video = c('.video');
-    video.style.opacity = 0;
-    setTimeout(()=>{
-        video.classList.toggle('close');
-        video.style.opacity = 1;
-    },500)
+const frame = c(".video video");
+function openClose() {
+  const video = c(".video");
+  video.style.opacity = 0;
+  setTimeout(() => {
+    video.classList.toggle("close");
+    video.style.opacity = 1;
+  }, 500);
 }
 
-c('.btnVideo').addEventListener('click', ()=>{
-    c('.headerFixed').style.display = 'none'
-    openClose();
+c(".btnVideo").addEventListener("click", () => {
+  c(".headerFixed").style.display = "none";
+  openClose();
 });
-c('#closeVideo').addEventListener('click', ()=>{
-    c('.headerFixed').style.display = 'block'
+c("#closeVideo").addEventListener("click", () => {
+  c(".headerFixed").style.display = "block";
 
-    let src = frame.src;
-    frame.src = src;
-    openClose();
+  let src = frame.src;
+  frame.src = src;
+  openClose();
 });
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+setInterval(function(){
+    nextImage();
+},2500)
+
+function nextImage() {
+  count++;
+  if(count > 4) {
+    count = 1;
+  }
+  document.getElementById("radio"+ count).checked = true;
+}
